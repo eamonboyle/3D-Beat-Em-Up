179aa2aa9b366d72e4cb1f90f3c1da92d6e9bf35
eamonboyle <blaowskate@hotmail.com>
2020-03-21 00:11:08 +0000

    Added enemy and attack points

---
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    private CharacterAnimation enemyAnim;
+
+    private Rigidbody myBody;
+    public float speed = 5f;
+
+    private Transform playerTarget;
+
+    public float attackDistance = 1f;
+    private float chasePlayerAfterAttack = 1f;
+
+    private float currentAttackTime;
+    private float defaultAttackTime = 2f;
+
+    private bool followPlayer, attackPlayer;
+
+    private void Awake()
+    {
+        enemyAnim = GetComponentInChildren<CharacterAnimation>();
+        myBody = GetComponent<Rigidbody>();
+
+        playerTarget = GameObject.FindWithTag(Tags.PLAYER_TAG).transform;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        followPlayer = true;
+        currentAttackTime = defaultAttackTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Attack();
+    }
+
+    private void FixedUpdate()
+    {
+        FollowTarget();
+    }
+
+    void FollowTarget()
+    {
+        if (!followPlayer)
+        {
+            return;
+        }
+
+        if (Vector3.Distance(transform.position, playerTarget.position) > attackDistance)
+        {
+            transform.LookAt(playerTarget);
+            myBody.velocity = transform.forward * speed;
+
+            if (myBody.velocity.sqrMagnitude != 0)
+            {
+                enemyAnim.Walk(true);
+            }
+        }
+        else if (Vector3.Distance(transform.position, playerTarget.position) <= attackDistance)
+        {
+            myBody.velocity = Vector3.zero;
+            enemyAnim.Walk(false);
+
+            followPlayer = false;
+            attackPlayer = true;
+        }
+    }
+
+    void Attack()
+    {
+        if (!attackPlayer)
+        {
+            return;
+        }
+
+        currentAttackTime += Time.deltaTime;
+
+        if (currentAttackTime > defaultAttackTime)
+        {
+            enemyAnim.EnemyAttack(Random.Range(0, 3));
+
+            currentAttackTime = 0f;
+        }
+
+        if (Vector3.Distance(transform.position, playerTarget.position) > attackDistance + chasePlayerAfterAttack)
+        {
+            attackPlayer = false;
+            followPlayer = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Player/CharacterAnimationDelegate.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimationDelegate : MonoBehaviour
+{
+    public GameObject leftArmAttackPoint;
+    public GameObject rightArmAttackPoint;
+    public GameObject leftLegAttackPoint;
+    public GameObject rightLegAttackPoint;
+
+    void LeftArmAttackOn()
+    {
+        leftArmAttackPoint.SetActive(true);
+    }
+
+    void LeftArmAttackOff()
+    {
+        if (leftArmAttackPoint.activeInHierarchy)
+        {
+            leftArmAttackPoint.SetActive(false);
+        }
+    }
+
+    void RightArmAttackOn()
+    {
+        rightArmAttackPoint.SetActive(true);
+    }
+
+    void RightArmAttackOff()
+    {
+        if (rightArmAttackPoint.activeInHierarchy)
+        {
+            rightArmAttackPoint.SetActive(false);
+        }
+    }
+
+    void LeftLegAttackPointOn()
+    {
+        if (leftLegAttackPoint.activeInHierarchy)
+        {
+            leftLegAttackPoint.SetActive(false);
+        }
+    }
+
+    void RightLegAttackPointOn()
+    {
+        if (rightLegAttackPoint.activeInHierarchy)
+        {
+            rightLegAttackPoint.SetActive(false);
+        }
+    }
+}
